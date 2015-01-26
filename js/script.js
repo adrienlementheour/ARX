9cb54d65aee8518fbc0f5a5826dbf4d84cbbed73
adrienlementheour <adrien.le.mentheour@gmail.com>
2015-01-26 17:55:31 +0100

    Bloc video youtube api

--- js/script.js
+++ js/script.js
@@ -24,6 +24,26 @@ function posiImgsReferences(){
 	});
 }
 
+// Fonction pour initialiser l'iframe youtube avec l'api
+function youtubeInit(){
+
+}
+
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Create YouTube player(s) after the API code downloads.
+var player1;
+var player2;
+
+function onYouTubeIframeAPIReady() {
+    player1 = new YT.Player('player-1');
+    player2 = new YT.Player('player-2');
+}
+
 $("ul#liste-references li img").load(function(){
 	posiImgsReferences();
 });
@@ -31,9 +51,14 @@ $("ul#liste-references li img").load(function(){
 $(document).ready(function(){
 	// Clic sur le bouton play d'une video
 	$("ul#slider-videos li#current-video a.video-cover").click(function() {
-		alert("aze")
+		// masquer le titre et le cover
+		TweenMax.set($(this), {display: "none"});
+		// lancer la video
+		player1.playVideo();
 		return false;
 	});
+	
+	youtubeInit();
 });
 
 $( window ).resize(function() {
